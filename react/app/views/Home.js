00bc95d20719f6b211160da071dff0c4be44b331
Herwt-liyj <oatostest02@126.com>
2019-12-04 00:44:21 +0800

    路由参数跳转

--- react/app/views/Home.js
+++ react/app/views/Home.js
@@ -4,10 +4,19 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <h1>Home页面</h1>
-                <h1>Login</h1>
-                <h1>List</h1>
-                <h1>Detail</h1>
+                <h1>
+                    <a href="#/home">Home页面</a></h1>
+                <h1>
+                    <button onClick={
+                        () => this.props.history.push({
+                            pathname: '/login',
+                            state: {
+                                user: 'Herwt'
+                            }
+                        })}>goLogin登录页面</button>
+                </h1>
+                <h1><button onClick={() => this.props.history.push('list')}>通过函数跳转</button></h1>
+                <h1><a href="#/detail">Detail页面</a></h1>
             </div>
         )
     }
--- react/app/views/Login.js
+++ react/app/views/Login.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+
+    componentDidMount() {
+        //console.log(this.props.match.params);
+        console.log(this.props.history.location.state);
+    }
+
+    
     render() {
         return (
             <h1>Login</h1>
         )
     }
+    
+    
 }
 
 export default Login;
